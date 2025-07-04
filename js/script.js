793f5029061861e65d351cad41811fb3b78be60d
Nanthawat Jommoi <oven08182546@gmail.com>
2025-07-04 20:39:01 +0700

    แก about, contact และ milky ใหแสดง scroll ได

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
 const searchInput = document.getElementById("search");
 const coffeeCards = document.querySelectorAll(".coffee-card");
 
-searchInput.addEventListener("input", () => {
-  const keyword = searchInput.value.toLowerCase();
-  coffeeCards.forEach(card => {
-    const name = card.querySelector("h3").textContent.toLowerCase();
-    card.classList.toggle("hidden", !name.includes(keyword));
+if (searchInput) {
+  searchInput.addEventListener("input", () => {
+    const keyword = searchInput.value.toLowerCase();
+    coffeeCards.forEach(card => {
+      const name = card.querySelector("h3").textContent.toLowerCase();
+      card.classList.toggle("hidden", !name.includes(keyword));
+    });
   });
-});
+}
+
+const toggleButton = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+if (toggleButton && navLinks) {
+  toggleButton.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
+}
+
